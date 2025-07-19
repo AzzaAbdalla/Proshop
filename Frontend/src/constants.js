019198bae15702ecac015064f86e264c56582b19
azzaSamy <azza.samy@nowpay.com>
2025-07-19 19:20:00 +0300

    Solved redirecting issue

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,8 +1,10 @@
 export const BASE_URL =
-  process.env.NOE_ENV === "development" ? "http://localhost:5000" : "";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : process.env.REACT_APP_BASE_URL || "";
 // export const BASE_URL = "";
 export const PRODUCTS_URL = "http://localhost:5000/api/products";
 export const USER_URL = "http://localhost:5000/api/users";
 export const ORDERS_URL = "http://localhost:5000/api/orders";
 export const PAYPAL_URL = "http://localhost:5000/api/config/paypal";
-export const UPLOADL_URL = "http://localhost:5000/api/upload";
+export const UPLOAD_URL = "http://localhost:5000/api/upload";
